40924797d0f40d69ec6bb41d098a0c39c85b4b96
Charlie Wood <charliemowood@gmail.com>
2017-06-12 18:16:23 +0100

    Add bogof status function and test to Cheese class

--- cheese.rb
+++ cheese.rb
@@ -2,10 +2,15 @@ class Cheese
 
   def initialize
     @price = 5
+    @bogof = false
   end
 
   def getPrice
     @price
+  end
+
+  def getBogofStatus
+    @bogof
   end
 
 end
--- specs/cheese_spec.rb
+++ specs/cheese_spec.rb
@@ -10,4 +10,8 @@ class TestCheese < Minitest::Test
    def test_get_price
      assert_equal(5, @cheese.getPrice)
    end
+
+   def test_get_bogof_status
+     assert_equal(false, @cheese.getBogofStatus)
+   end
 end
