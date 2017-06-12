81337e0311cadde32860cd2536143cd9a27e0b2a
Charlie Wood <charliemowood@gmail.com>
2017-06-12 22:34:43 +0100

    Add loyality card discount functionality to checkout class

--- checkout.rb
+++ checkout.rb
@@ -1,17 +1,20 @@
+require_relative './customer'
 class Checkout
 
   def initialize
     @balance = 0
     @bogof_discount = 0
     @over20_discount = 0
-    @total = 0
+    @subtotal = 0
+    @loyality_card_discount = 0
+    @grandtotal = 0
   end
 
   def setBogof_discount(discount)
     @bogof_discount = discount
   end
 
-  def getBalance(basket)
+  def getBalance(basket, customer)
 
     array = basket.getItemArray
 
@@ -26,7 +29,13 @@ class Checkout
     if @total > 20
       @over20_discount = @total * 0.1
     end
-    return @total - @over20_discount
+
+    if customer.checkLoyalityCardStatus == true
+      @loyality_card_discount = @total * 0.02
+    end
+
+    @grandtotal = @total - @over20_discount - @loyality_card_discount
+    return @grandtotal
   end
 
 end
--- specs/checkout_spec.rb
+++ specs/checkout_spec.rb
@@ -3,6 +3,7 @@ require_relative '../checkout'
 require_relative '../basket'
 require_relative '../bread'
 require_relative '../cheese'
+require_relative '../customer'
 
 
 class TestCheckout < Minitest::Test
@@ -12,16 +13,18 @@ class TestCheckout < Minitest::Test
      @basket = Basket.new()
      @cheese = Cheese.new()
      @bread = Bread.new()
+     @customer = Customer.new(true)
+     @customer1 = Customer.new(false)
    end
 
    def test_balance_start_0
-     assert_equal(0, @checkout.getBalance(@basket))
+     assert_equal(0, @checkout.getBalance(@basket, @customer1))
    end
 
    def test_can_get_balance_total_from_basket
      @basket.add_item(@cheese)
      @basket.add_item(@bread)
-     assert_equal(8, @checkout.getBalance(@basket))
+     assert_equal(8, @checkout.getBalance(@basket, @customer1))
    end
 
    def test_can_different_total_from_basket
@@ -29,7 +32,7 @@ class TestCheckout < Minitest::Test
      @basket.add_item(@bread)
      @basket.add_item(@bread)
      @basket.add_item(@cheese)
-     assert_equal(16, @checkout.getBalance(@basket))
+     assert_equal(16, @checkout.getBalance(@basket, @customer1))
    end
 
    def test_does_over_20_discount_work
@@ -38,7 +41,7 @@ class TestCheckout < Minitest::Test
      @basket.add_item(@cheese)
      @basket.add_item(@cheese)
      @basket.add_item(@cheese)
-     assert_equal(22.5, @checkout.getBalance(@basket))
+     assert_equal(22.5, @checkout.getBalance(@basket, @customer1))
    end
 
    def test_10_breads_over_20_discount_works
@@ -52,7 +55,29 @@ class TestCheckout < Minitest::Test
      @basket.add_item(@bread)
      @basket.add_item(@bread)
      @basket.add_item(@bread)
-     assert_equal(27, @checkout.getBalance(@basket))
+     assert_equal(27, @checkout.getBalance(@basket, @customer1))
+   end
+
+   def test_loyality_card_owner_gets_2percent_off
+     @basket.add_item(@cheese)
+     @basket.add_item(@cheese)
+     assert_equal(9.8, @checkout.getBalance(@basket, @customer))
+   end
+
+   def test_loyality_bogof_andover20_discount
+     @basket.add_item(@bread)
+     @basket.add_item(@bread)
+     @basket.add_item(@bread)
+     @basket.add_item(@bread)
+     @basket.add_item(@bread)
+     @basket.add_item(@bread)
+     @basket.add_item(@bread)
+     @basket.add_item(@bread)
+     @basket.add_item(@bread)
+     @basket.add_item(@bread)
+     @basket.add_item(@cheese)
+     @basket.add_item(@cheese)
+     assert_equal(35.2, @checkout.getBalance(@basket, @customer))
    end
 
 
